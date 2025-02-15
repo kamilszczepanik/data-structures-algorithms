85cbaae9de5aede2aacdcf04747c00616afd2e6a
Kamil Szczepanik <kszczepanikworkspace@gmail.com>
2025-02-15 19:23:15 +0400

    feat: first recurring character checkers

--- hash-table/first-recurring-character.js
+++ hash-table/first-recurring-character.js
@@ -6,6 +6,14 @@ function firstRecurringCharacter(input) {
   //* possible solution - nested arrays - O(n^2) - bad solution
 
   // better solution - O(n) linear
+  if (
+    !input ||
+    input.length === 0 ||
+    !input.every((element) => typeof element === "number")
+  ) {
+    throw new Error("Provide correct input");
+  }
+
   const set = new Set();
 
   for (let i = 0; i < input.length; i++) {
@@ -19,7 +27,7 @@ function firstRecurringCharacter(input) {
   return undefined;
 }
 
-console.log(firstRecurringCharacter([2, 5, 1, 2, 3, 5, 1, 2, 4])); // should return 2
+console.log(firstRecurringCharacter([2, 5, 1, 2, 3, 5, 1, 2, undefined])); // should return 2
 console.log(firstRecurringCharacter([2, 1, 1, 2, 3, 5, 1, 2, 4])); // should return 1
 console.log(firstRecurringCharacter([2, 3, 4, 5])); // should return undefined
 console.log(firstRecurringCharacter([2, 5, 5, 2, 3, 5, 1, 2, 4])); // should return 5
