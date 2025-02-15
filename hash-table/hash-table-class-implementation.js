d27c6e5474613e23af88d247228a1d1163cfdcf3
Kamil Szczepanik <kszczepanikworkspace@gmail.com>
2025-02-15 18:51:31 +0400

    feat: implementation of keys in hash table

--- hash-table/hash-table-class-implementation.js
+++ hash-table/hash-table-class-implementation.js
@@ -34,10 +34,24 @@ class HashTable {
     }
     return undefined;
   } // O(1) - if no collision, when collision multiple times it will be O(n)
+
+  keys() {
+    const keys = [];
+    const items = this.data.filter((item) => item);
+
+    for (let i = 0; i < items.length; i++) {
+      for (let j = 0; j < items[i].length; j++) {
+        keys.push(items[i][j][0]);
+      }
+    }
+
+    return keys;
+  } // O(n) - even slower because of the filter and double
 }
 
-const myHashTable = new HashTable(50);
+const myHashTable = new HashTable(2);
 myHashTable.set("grapes", 10000);
 myHashTable.set("apples", 10000);
 myHashTable.set("kiwi", 10000);
 myHashTable.get("grapes");
+console.log("keys", myHashTable.keys());
